158e9bc9787871f9ab51e2b864d7c5165d534b0f
Ifan Ashari <ifanashari1234@gmail.com>
2019-03-20 20:46:39 +0700

    Initial commit

---
+++ application/controllers/Dashboard_C.php
@@ -0,0 +1,16 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Dashboard_C extends CI_Controller {
+    
+        public function index()
+        {
+            
+        }
+    
+    }
+    
+    /* End of file Dashboard_C.php */
+    
+?>
